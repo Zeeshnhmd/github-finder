18bbd7160267486f17d75c95ecdd7ce89f2330e7
Zeeshan Ahmad <zeeshnhmd.1@gmail.com>
2022-07-08 22:00:49 +0530

    added skeleton for users

--- src/components/users/UserList.jsx
+++ src/components/users/UserList.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import { useContext } from 'react';
+import GithubContext from '../../context/github/GithubContext';
 import { Link } from 'react-router-dom';
+import Skeleton from '@mui/material/Skeleton';
 
 const UserList = ({ person: { login, avatar_url } }) => {
-	return (
+	const { loading } = useContext(GithubContext);
+	return loading ? (
+		<div className="card shadow-md compact bg-base-100">
+			<div className="flex-row items-center space-x-4 card-body">
+				<Skeleton variant="circular" width={50} height={50} />
+				<div className="flex-col w-3/4 ">
+					<Skeleton variant="text" width="60%" style={{ marginBottom: 6 }} />
+					<Skeleton variant="text" width="30%" />
+				</div>
+			</div>
+		</div>
+	) : (
 		<div className="card shadow-md compact side bg-base-100  transform transition duration-500 hover:scale-105">
 			<div className="flex-row items-center space-x-4 card-body">
 				<div className="avatar">
--- src/components/users/UserResult.jsx
+++ src/components/users/UserResult.jsx
@@ -1,18 +1,11 @@
 import { useContext } from 'react';
 import GithubContext from '../../context/github/GithubContext';
 import UserList from './UserList';
-import { RevolvingDot } from 'react-loader-spinner';
-
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 const UserResult = () => {
-	const { users, loading } = useContext(GithubContext);
+	const { users } = useContext(GithubContext);
 
-	return loading ? (
-		<div className="flex justify-center">
-			<RevolvingDot color="#ffffff" height={100} width={100} />
-		</div>
-	) : (
+	return (
 		<div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
 			{users.map((user) => {
 				return <UserList key={user.id} person={user} />;

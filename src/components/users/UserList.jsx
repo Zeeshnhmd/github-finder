3f554155b1382e7ff22bf648ce031d813d7505fe
Zeeshan Ahmad <zeeshnhmd.1@gmail.com>
2022-07-20 20:48:01 +0530

    sekleton update

--- src/components/users/UserList.jsx
+++ src/components/users/UserList.jsx
@@ -5,28 +5,48 @@ import Skeleton from '@mui/material/Skeleton';
 
 const UserList = ({ person: { login, avatar_url } }) => {
 	const { loading } = useContext(GithubContext);
-	return loading ? (
-		<div className="card shadow-md compact  bg-secondary">
-			<div className="flex-row items-center space-x-4 card-body">
-				<Skeleton variant="circular" width={50} height={50} />
-				<div className="flex-col w-3/4 ">
-					<Skeleton variant="text" width="60%" style={{ marginBottom: 6 }} />
-					<Skeleton variant="text" width="30%" />
-				</div>
-			</div>
-		</div>
-	) : (
+
+	return (
 		<div className="card shadow-md compact side bg-secondary transform transition duration-500 hover:scale-105">
 			<div className="flex-row items-center space-x-4 card-body">
 				<div className="avatar">
 					<div className="rounded-full shadow w-14 h-14">
-						<img src={avatar_url} alt="avatar" />
+						{loading ? (
+							<Skeleton
+								animation="wave"
+								variant="circular"
+								width={50}
+								height={50}
+							/>
+						) : (
+							<img src={avatar_url} alt="avatar" />
+						)}
 					</div>
 				</div>
 				<div>
-					<h1 className="pb-1 text-neutral card-title">{login}</h1>
+					<h1 className="pb-1 text-neutral card-title">
+						{loading ? (
+							<Skeleton
+								variant="wave"
+								height={13}
+								width={100}
+								style={{ marginBottom: 6 }}
+							/>
+						) : (
+							login
+						)}
+					</h1>
 					<Link className="text-neutral" to={`/user/${login}`}>
-						Visit Profile
+						{loading ? (
+							<Skeleton
+								variant="wave"
+								height={10}
+								width={50}
+								style={{ marginBottom: 6 }}
+							/>
+						) : (
+							'Visit Profile'
+						)}
 					</Link>
 				</div>
 			</div>
